635f9d74205fa1fb6eddce9d79530358fca25561
ttak0422 <bgdaewalkman@gmail.com>
2019-07-06 22:49:52 +0900

    update repo

--- src/page/Repo.fs
+++ src/page/Repo.fs
@@ -1,12 +1,12 @@
 module Sample.Page.Repo
 
 open Elmish
+open Fulma
 open Fable.React
 open Fable.React.Props
 open GitHub
 
 // Type
-
 type State =
     | Init
     | Loaded of Issue list
@@ -21,36 +21,51 @@ type Msg =
     | Receive of Result<Issue list, string>
     | ReceiveErr of exn
 
-
 // State
-
 let init userName projectName =
     let model =
         { UserName = userName
           ProjectName = projectName
           State = Init }
-    let cmd = Cmd.OfAsync.either getIssues (userName, projectName) Receive ReceiveErr
+
+    let cmd =
+        Cmd.OfAsync.either getIssues (userName, projectName) Receive ReceiveErr
     model, cmd
 
 let update msg model : Model * Cmd<Msg> =
     match msg with
-    | Receive (Ok issues) -> { model with State = Loaded issues }, Cmd.none
-    | Receive (Error e) -> { model with State = Failed <| failwithf "%s" e}, Cmd.none
-    | ReceiveErr e -> { model with State = Failed e}, Cmd.none
-
+    | Receive(Ok issues) -> { model with State = Loaded issues }, Cmd.none
+    | Receive(Error e) ->
+        { model with State = Failed <| failwithf "%s" e }, Cmd.none
+    | ReceiveErr e -> { model with State = Failed e }, Cmd.none
 
 // View
-
 let root model dispatch =
     let viewIssues userName projectName (issue : Issue) =
         li []
             [ span [] [ str <| "[" + issue.State + "]" ]
-              a
-                [ Href <| "https://github.com/" + userName + "/" + projectName + "/issues/" + (string issue.Number)
-                  Target "_blank" ]
-                [ str <| sprintf "#%d %s" issue.Number issue.Title ] ]
 
+              a [ Href
+                  <| "https://github.com/" + userName + "/" + projectName
+                     + "/issues/" + (string issue.Number)
+                  Target "_blank" ]
+                  [ str <| sprintf "#%d %s" issue.Number issue.Title ] ]
     match model.State with
-    | Init -> str "loading..."
-    | Loaded issues -> ul [] (List.map (viewIssues model.UserName model.ProjectName) issues)
+    | Init -> Utils.loader
     | Failed e -> str <| string e
+    | Loaded issues ->
+        let projectName =
+            Box.box' [ Modifiers [ Modifier.BackgroundColor IsPrimary ] ]
+                [ Text.p
+                      [ Modifiers
+                            [ Modifier.TextWeight TextWeight.Bold
+                              Modifier.TextSize(Screen.All, TextSize.Is3) ] ]
+                      [ str <| sprintf "%s/%s" model.UserName model.ProjectName ] ]
+        Box.box' []
+            [ projectName
+
+              Text.p [ Modifiers [ Modifier.TextWeight TextWeight.Bold ] ]
+                  [ str "Issues" ]
+
+              ul []
+                  (List.map (viewIssues model.UserName model.ProjectName) issues) ]
